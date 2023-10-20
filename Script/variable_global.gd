796011e7848aa474b45283cb7de2c55695d1d0f7
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-10-20 16:45:59 +0200

    Initialisation des tableaux d'attaque et test fonctionnalité

--- Script/variable_global.gd
+++ Script/variable_global.gd
@@ -1,22 +1,36 @@
 extends Node
-var one_move_case = 100
-var turnWhite = true
 
-# Created chessboard
 var chessBoard = []
 var piece_white = [null,null,"RookWhite","KnightWhite","BishopWhite","QueenWhite","KingWhite","BishopWhite2","KnightWhite2","RookWhite2"]
 var piece_black = [null,null,"RookBlack","KnightBlack","BishopBlack","QueenBlack","KingBlack","BishopBlack2","KnightBlack2","RookBlack2"]
+var attack_piece_white_on_the_chessboard = []
+var attack_piece_black_on_the_chessboard = []
+
+var one_move_case = 100
+var turnWhite = true
+var update_of_the_parts_attack = false
 
 func _ready():
 	createBoard(12,12)
 	initialisingChessBoard()
+	createAttackBoardWhiteAndBlack(12,12)
+	initialisingAttackBoardWhiteAndBlack()
 
 func _process(delta):
-#	if turnWhite == true:
+	if turnWhite == true:
 #		get_node("/root/ChessBoard/Camera2D").set_rotation_degrees(0)
-#	else:
+		if update_of_the_parts_attack == false:
+			updateAttackWhiteandBlack(turnWhite)
+			attack_pieces_black()
+			
+			update_of_the_parts_attack = true
+			
+	elif turnWhite == false:
 #		get_node("/root/ChessBoard/Camera2D").set_rotation_degrees(180)
-	pass
+		if update_of_the_parts_attack == true:
+			updateAttackWhiteandBlack(turnWhite)
+			attack_pieces_white()
+			update_of_the_parts_attack = false
 
 func createBoard(rowSize,columnSize):
 	for i in range(rowSize):
@@ -24,9 +38,8 @@ func createBoard(rowSize,columnSize):
 		for j in range(columnSize):
 			row.append(null)
 		chessBoard.append(row)
-	
-#	print(chessBoard)
-#	print("ChessBoard created. Size: ", rowSize, "x", columnSize)
+	#	print(chessBoard)
+	#	print("ChessBoard created. Size: ", rowSize, "x", columnSize)
 
 func initialisingChessBoard():
 	for i in range(2,3):
@@ -46,8 +59,87 @@ func initialisingChessBoard():
 			chessBoard[i][j] = piece_white[j]
 	for i in range(0,12):
 		for j in range(0,12):
-			if chessBoard[i][j] ==  null :
+			if chessBoard[i][j] ==  null:
 				chessBoard[i][j] = "x"
 	
+	print("ChessBoard: ")
 	for i in range(0,12):
 		print(chessBoard[i])
+
+func createAttackBoardWhiteAndBlack(rowSize,columnSize):
+	for i in range(rowSize):
+		var row = []
+		for j in range(columnSize):
+			row.append(null)
+		attack_piece_white_on_the_chessboard.append(row)
+		
+	for i in range(rowSize):
+		var row = []
+		for j in range(columnSize):
+			row.append(null)
+		attack_piece_black_on_the_chessboard.append(row)
+	#	print(attack_piece_white_on_the_chessboard)
+	#	print(attack_piece_black_on_the_chessboard)
+
+func initialisingAttackBoardWhiteAndBlack():
+	for i in range(2,10): 
+		for j in range(2,10):
+			attack_piece_white_on_the_chessboard[i][j] = 0
+			attack_piece_black_on_the_chessboard[i][j] = 0
+
+	for i in range(0,12):
+		for j in range(0,12):
+			if attack_piece_white_on_the_chessboard[i][j] ==  null:
+				attack_piece_white_on_the_chessboard[i][j] = -9
+	
+	for i in range(0,12):
+		for j in range(0,12):
+			if attack_piece_black_on_the_chessboard[i][j] ==  null:
+				attack_piece_black_on_the_chessboard[i][j] = -9
+	
+	printAttackWhite()
+	printAttackBlack()
+
+func updateAttackWhiteandBlack(color):
+	if color == true:
+		for i in range(2,10): 
+			for j in range(2,10):
+				attack_piece_white_on_the_chessboard[i][j] = 0
+	else:
+		for i in range(2,10): 
+			for j in range(2,10):
+				attack_piece_black_on_the_chessboard[i][j] = 0
+
+func printAttackWhite():
+	print("AttackBoardWhite: ")
+	for i in range(0,12):
+		print(attack_piece_white_on_the_chessboard[i])
+
+func printAttackBlack():
+	print("AttackBoardBlack: ")
+	for i in range(0,12):
+		print(attack_piece_black_on_the_chessboard[i])
+
+func attack_pieces_white():
+	for i in range(12):
+		for j in range(12):
+			if chessBoard[i][j] == "PawnWhite":
+				if chessBoard[i-1][j+1] != "x":
+					attack_piece_white_on_the_chessboard[i-1][j+1] += 1
+					
+				if chessBoard[i-1][j-1] != "x":
+					attack_piece_white_on_the_chessboard[i-1][j-1] += 1
+					
+	printAttackWhite()
+
+func attack_pieces_black():
+	for i in range(12):
+		for j in range(12):
+			if chessBoard[i][j] == "PawnBlack":
+				if chessBoard[i+1][j+1] != "x":
+					attack_piece_black_on_the_chessboard[i+1][j+1] += 1
+					
+				if chessBoard[i+1][j-1] != "x":
+					attack_piece_black_on_the_chessboard[i+1][j-1] += 1
+					
+	printAttackBlack()
